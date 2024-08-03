a777b8801464862ea85107aeb7c61ac9c106e1ff
z.razzhivin <z.razzhivin@tinkoff.ru>
2024-08-03 11:00:33 +0300

    add sql scripts

---
+++ School/scripts.sql
@@ -0,0 +1,7 @@
+select * from student;
+
+select * from student where age between 10 and 20;
+select name from student;
+select * from student where name like '%o%';
+select * from student where age < id;
+select * from student order by age;
